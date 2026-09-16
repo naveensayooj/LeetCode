3c871085a483122fdbd3bffcd372cd94986e3f27
naveen kp <naveen1050kp@gmail.com>
2026-09-16 10:45:44 +0530

    Time: 56 ms (21.15%) | Memory: 54.4 MB (71.15%) - LeetSync

---
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var returnToBoundaryCount = function(nums) {
+    let position=0;
+    let count=0;
+    for(let i=0;i<=nums.length;i++)
+    {
+        position+=nums[i];
+        if(position===0)
+        {
+            count++;
+        }   
+    }
+     return count;
+};
