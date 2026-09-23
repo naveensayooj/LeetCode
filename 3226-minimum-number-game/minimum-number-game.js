0094f59cf4c376de1bfd60db911304a52a23506f
naveen kp <naveen1050kp@gmail.com>
2026-09-23 11:06:35 +0530

    Time: 3 ms (49.73%) | Memory: 56.3 MB (80.54%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function numberGame(nums) {
+    nums.sort((a, b) => a - b);
+
+    let arr = [];
+
+    for (let i = 0; i < nums.length; i += 2) {
+        arr.push(nums[i + 1]);
+        arr.push(nums[i]);
+    }
+
+    return arr;
+}
